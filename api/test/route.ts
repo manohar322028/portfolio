90fb61cca9798792b95f94ba5c24351f85d3bd81
manohar322028 <manohardahal40@gmail.com>
2024-03-21 15:56:16 +0545

    made production ready

---
+++ api/test/route.ts
@@ -0,0 +1,8 @@
+// import { dbConnect } from "@/backend/db.js";
+// import { NextResponse } from "next/server";
+// import { Skill } from "@/backend/models/skill.ts";
+// export async function GET() {
+//   dbConnect();
+//   const skills = await Skill.find();
+//   return NextResponse.json(skills);
+// }

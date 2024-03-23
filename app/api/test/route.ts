eeed2165061d29d580dc7dcbe00e2eb3357fa2fe
manohar322028 <manohardahal40@gmail.com>
2024-03-23 12:33:44 +0545

    edited a little

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -2,7 +2,7 @@ import { dbConnect } from "@/backend/db.ts";
 import { NextResponse } from "next/server";
 import { Skill } from "@/backend/models/skill.ts";
 export async function GET() {
-  dbConnect();
+  await dbConnect();
   const skills = await Skill.find();
   return NextResponse.json(skills);
 }
